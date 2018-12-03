3e61658446246bcf4afa05fa5cdcba6b9dcb5a4f
Toru Makabe <tomakabe@tomakabe-mbp2018-13.guest.corp.microsoft.com>
2018-12-03 18:26:38 +0900

    add ContainerInsights solution to shared resource

--- chap08-12-all-in-one/shared/main.tf
+++ chap08-12-all-in-one/shared/main.tf
@@ -65,6 +65,19 @@ resource "azurerm_log_analytics_workspace" "shared" {
   sku                 = "Free"
 }
 
+resource "azurerm_log_analytics_solution" "shared" {
+  solution_name         = "ContainerInsights"
+  location              = "${azurerm_resource_group.shared.location}"
+  resource_group_name   = "${azurerm_resource_group.shared.name}"
+  workspace_resource_id = "${azurerm_log_analytics_workspace.shared.id}"
+  workspace_name        = "${azurerm_log_analytics_workspace.shared.name}"
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+}
+
 resource "azurerm_monitor_action_group" "critical" {
   name                = "Critical"
   resource_group_name = "${azurerm_resource_group.shared.name}"
