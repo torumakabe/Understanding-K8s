4e2d4ebce79a13bc8035e2bfb78e7b42a50591cc
ToruMakabe <tomakabe@microsoft.com>
2018-12-05 17:20:15 +0900

    devide deploy and cleanup to isolate risk

---
+++ chap08-12-all-in-one/cluster-blue/cleanup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CHAP="aio"
+
+# Destroy AKS Clster (Blue)
+terraform init \
+    -backend-config="storage_account_name=${TF_VAR_k8sbook_prefix}${CHAP}tfstate" \
+    -backend-config="container_name=tfstate-cluster-blue" \
+    -backend-config="key=terraform.tfstate" \
+    -reconfigure \
+    ./cluster-blue
+
+terraform destroy -auto-approve ./cluster-blue
+
+# Delete Kubetenetes config
+rm ~/.kube/${TF_VAR_k8sbook_prefix}-k8sbook-${CHAP}-aks-blue-primary-config
+rm ~/.kube/${TF_VAR_k8sbook_prefix}-k8sbook-${CHAP}-aks-blue-failover-config
